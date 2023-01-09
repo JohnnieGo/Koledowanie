331da19ed66b78b95bb528241c5ecc3f656cdc87
Jan <osiem4321@gmail.com>
2023-01-09 02:06:21 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,35 @@
+import React, { useEffect, useState } from "react";
+
 import Picture from "./components/Picture";
 import Content from "./components/Content";
 import About from "./components/About";
-import { Animate, AnimateKeyframes, AnimateGroup } from "react-simple-animate";
-import { Link, Route, Switch } from "wouter";
+import { Link, Route, Router, Switch, useLocation, useRoute  } from "wouter";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+
 import "./App.css";
 
 function App() {
+  const nodeRef = React.useRef(null);
+  const location = useLocation();
+  const [match, params] = useRoute("/koledowanie/o-nas");
+
+
   return (
-    <div className="w-full flex flex-col max-lg:gap-4 lg:grid grid-cols-2 h-screen box-border ">
-      <div className=" flex justify-center items-center px-16 2xl:px-32 xl:px-24 lg:px-32 md:px-56 sm:px-32 ">
-        <Picture />
-      </div>
-      <div className="w-full h-full p-10">
-        <div className="border bg-bige w-full h-full p-10 overflow-auto">
-          <Switch>
-            <Route path="/witajcie">
-              <Content />
-            </Route>
-            <Route path="/o-nas">
-                <About />
-            </Route>
-          </Switch>
+    <Router base="/koledowanie">
+      <div className="w-full flex flex-col max-lg:gap-4 lg:grid grid-cols-2 h-screen box-border ">
+        <div className=" flex justify-center items-center px-16 2xl:px-32 xl:px-24 lg:px-32 md:px-56 sm:px-32 ">
+          <Picture />
+        </div>
+        <div className="w-full h-full p-10">
+          <div className="border bg-bige w-full h-full p-10 overflow-hidden">
+                <Switch>
+                  <Route path="/" component={Content} />
+                  <Route path="/o-nas" component={About} />
+                </Switch>
+          </div>
         </div>
       </div>
-    </div>
+    </Router>
   );
 }
 export default App;
